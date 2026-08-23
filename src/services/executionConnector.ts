5c0e458d967261be4a3679dc4afa48cc1822a7e0
temeaco-max <temea.co@gmail.com>
2026-08-23 12:57:16 +0100

    persistence(execution): route facade directly to canonical persistence owner

--- src/services/executionConnector.ts
+++ src/services/executionConnector.ts
@@ -1,24 +1,3 @@
-import { getCanonicalPersistenceMode } from './canonicalPersistence.js';
-import * as sqljs from './executionConnectorSqljs.js';
-import * as postgres from './executionPersistence.js';
-export type { ConnectorAdapter, ExecutionEvidence, ExecutionRequestRecord, ExecutionStatus, EvidenceSource, EvidenceVerificationState } from './executionConnectorSqljs.js';
-export { ExecutionAuthorizationError, ExecutionConflictError } from './executionConnectorSqljs.js';
-export const EXECUTION_STATUSES = sqljs.EXECUTION_STATUSES;
-export const EVIDENCE_SOURCES = sqljs.EVIDENCE_SOURCES;
-export const EVIDENCE_VERIFICATION_STATES = sqljs.EVIDENCE_VERIFICATION_STATES;
-export const dummyTestConnector = sqljs.dummyTestConnector;
-export function registerConnector(adapter: sqljs.ConnectorAdapter){return sqljs.registerConnector(adapter)}
-export function getConnector(id:string){return sqljs.getConnector(id)}
-function impl<T extends keyof typeof sqljs & keyof typeof postgres>(name:T):typeof sqljs[T]|typeof postgres[T]{return getCanonicalPersistenceMode()==='postgres'?(postgres[name] as any):(sqljs[name] as any)}
-export const authorizeProviderConnector=(i:Parameters<typeof sqljs.authorizeProviderConnector>[0])=>impl('authorizeProviderConnector')(i);
-export const revokeProviderConnector=(i:Parameters<typeof sqljs.revokeProviderConnector>[0])=>impl('revokeProviderConnector')(i);
-export const authorizeProviderExecution=(i:Parameters<typeof sqljs.authorizeProviderExecution>[0])=>impl('authorizeProviderExecution')(i);
-export const getExecutionRequest=(id:string)=>impl('getExecutionRequest')(id);
-export const getExecutionRequestsForRequest=(id:string)=>impl('getExecutionRequestsForRequest')(id);
-export const createExecutionRequest=(i:Parameters<typeof sqljs.createExecutionRequest>[0])=>impl('createExecutionRequest')(i);
-export const completeDevelopmentExecution=(id:string)=>impl('completeDevelopmentExecution')(id);
-export const dispatchExecutionRequest=(id:string)=>impl('dispatchExecutionRequest')(id);
-export const drainPendingExecutionRequests=(limit?:number)=>impl('drainPendingExecutionRequests')(limit);
-export const updateExecutionStatus=(id:string,status:sqljs.ExecutionStatus,patch?:Parameters<typeof sqljs.updateExecutionStatus>[2])=>impl('updateExecutionStatus')(id,status,patch);
-export const recordExecutionEvidence=(id:string,input:Parameters<typeof sqljs.recordExecutionEvidence>[1])=>impl('recordExecutionEvidence')(id,input);
-export const reviewExecutionEvidence=(i:Parameters<typeof sqljs.reviewExecutionEvidence>[0])=>impl('reviewExecutionEvidence')(i);
+export * from './executionPersistence.js';
+export type { ConnectorAdapter } from './executionConnectorSqljs.js';
+export { dummyTestConnector, registerConnector, getConnector } from './executionConnectorSqljs.js';
